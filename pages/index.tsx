e05f545da7f6076383a53387dfceae0c9ae8d480
Dario Didi <dididario.dd2@gmail.com>
2025-08-01 20:25:02 +0300

    feat: basic home page skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,13 @@
  * TIMELINE PAGE
  * */
 
-import Image from "next/image";
+import Layout from "@/components/layout/Layout";
 import { useState } from "react";
 
 
 const Home = () => {
-  const [limit] = useState(10);
+  // limits of num posts in timeline
+  const [limit, setLimit] = useState(10);
   const [offset, setOffset] = useState(0);
 
   //TODO: setup graphql query
@@ -15,8 +16,22 @@ const Home = () => {
   //TODO: handle likes, shares, comments(with modal)
 
 
-  return ()
-}
+  return (
+    <Layout>
+      <div className="max-w-2xl mx-auto">
+        {/*TODO: add post form in the timeline to enable new posts*/}
+        {/*LIST POSTS*/}
+        <div className="mt-4">
+
+        </div>
+
+        {/*TODO: Load more data button */}
+      </div>
+    </Layout>
+  )
+};
+
+export default Home;
 
 
 
